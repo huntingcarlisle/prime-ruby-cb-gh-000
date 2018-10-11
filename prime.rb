48e64237caa92a4ae3bd8792f94363475431e12b
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-11 20:09:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,9 @@ def prime?(integer, i = 2)
     return integer == 2
   elsif integer % i == 0
     return FALSE
+  elsif i * i > integer
+    return TRUE
   else
-    
+    return prime?(integer, i + 1)
   end
 end
