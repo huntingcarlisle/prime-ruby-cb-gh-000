fd7ec469432236b6da021f9ba1420ce3d3731cf3
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-11 20:09:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(integer, i = 2)
+  if integer <= 2
+    return integer == 2
+  elsif integer % i == 0
+    return FALSE
+  elsif i * i > integer
+    return TRUE
+  else
+    return prime?(integer, i + 1)
+  end
+end
